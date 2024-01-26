2be5b0dc544664d6ec5814ab48e98b6d83c7c437
rgraue <rgraue@outlook.com>
2024-01-25 17:14:47 -0800

    init dockerize

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const apiRoutes = require('./routes/api');
+// const apiRoutes = require('./routes/api');
 
 require('dotenv').config();
 
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
