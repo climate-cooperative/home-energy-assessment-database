f2923fb5e1379a5518941a074dcbc9a433991d32
rgraue <rgraue@outlook.com>
2024-03-21 20:44:45 -0700

    right image uir

--- terraform/main.tf.tf
+++ terraform/main.tf.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "test_lambda" {
   function_name = "zwell-test-lambda"
   role = aws_iam_role.iam_for_lambda.arn
-  image_uri = "${aws_ecr_repository.lambda_image_repo.repository_url}/${var.image_tag}"
+  image_uri = "${aws_ecr_repository.lambda_image_repo.repository_url}:${var.image_tag}"
   package_type = "Image"
   architectures = ["arm64"]
 }
