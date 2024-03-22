bfe15111bb30045857ae070a31a99d3ae15b941b
rgraue <rgraue@outlook.com>
2024-03-22 14:10:58 -0700

    redo api gw

--- terraform/main.tf.tf
+++ terraform/main.tf.tf
@@ -27,3 +27,13 @@ resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
+resource "aws_lambda_permission" "zwell-api-gw-permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.zwell-api-gateway.execution_arn}/*/*"
+}
