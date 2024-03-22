bf1c89bb2bcb5239565e51ba166efde10da31e73
rgraue <rgraue@outlook.com>
2024-03-21 17:42:57 -0700

    try

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,21 +6,13 @@ terraform {
     }
   }
 
-  cloud {
-    organization = "zwell-test"
-
-    workspaces {
-      name = "dev-test"
-    }
-  }
-
   required_version = "~> 1.7"
 
-  # backend "s3" {
-  #   bucket = "mxatqn6mjjtjqzwg"
-  #   key = "tf-state"
-  #   region = "us-west-2"
-  # }
+  backend "s3" {
+    bucket = "mxatqn6mjjtjqzwg"
+    key = "tf-state"
+    region = "us-west-2"
+  }
 }
 
 provider "aws" {
