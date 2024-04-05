a11ae7309e03a91b3e87e5ad6234b577b93fcccf
rgraue <rgraue@outlook.com>
2024-04-04 20:35:57 -0700

    update provider

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -1,4 +1,6 @@
+// zwell certs live in us-east-1...
 data "aws_acm_certificate" "acm_cert" {
+  provider = aws.us-east-1
   domain = "*.zwell.io"
   statuses = [ "ISSUED" ]
   types = [ "IMPORTED" ]
