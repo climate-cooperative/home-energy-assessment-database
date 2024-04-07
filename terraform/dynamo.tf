f8fa94a3a0f8e477acfb60f260f1b8d4906f0db2
rgraue <rgraue@outlook.com>
2024-04-07 14:01:04 -0700

    add hvac table

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -13,3 +13,19 @@ resource "aws_dynamodb_table" "zwell_appliance_table" {
           type = "S"
         }
 }
+
+resource "aws_dynamodb_table" "zwell_hvac_table" {
+  name = "prod_zwell_hvac_table"
+  billing_mode = "PAY_PER_REQUEST" // free < 1mil read/write ops/units... ALOT
+  hash_key = "_id" // primary key
+  range_key = "display_name" // sort key
+
+  attribute {
+        name = "_id"
+        type = "S"
+      }
+  attribute {
+          name = "display_name"
+          type = "S"
+        }
+}
