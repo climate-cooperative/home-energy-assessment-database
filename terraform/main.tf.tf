bec87c14fe90620b8afb822b9e98c264f80804c1
rgraue <rgraue@outlook.com>
2024-04-07 18:31:00 -0700

    add cloudwathc logging

--- terraform/main.tf.tf
+++ terraform/main.tf.tf
@@ -78,3 +78,34 @@ resource "aws_iam_role_policy_attachment" "zwell_data_api_attachment" {
   role = aws_iam_role.zwell_data_api_role.name
   policy_arn = aws_iam_policy.zwell_data_api_dynamo_policy.arn
 }
+
+// loggoing 
+resource "aws_iam_policy" "function_logging_policy" {
+  name   = "function-logging-policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        Action : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect : "Allow",
+        Resource : "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
+  role       = aws_iam_role.zwell_data_api_role.id
+  policy_arn = aws_iam_policy.function_logging_policy.arn
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/${var.lambda.function_name}"
+  retention_in_days = 7
+  lifecycle {
+    prevent_destroy = false
+  }
+}
