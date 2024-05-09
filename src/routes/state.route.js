e39a518ceb25752844bfcae08aed9c99bc207443
Simon Fishel <simonf@gmail.com>
2024-05-08 21:01:30 -0700

    adds endpoint to get state data from a zipcode
    Fixes #24

--- src/routes/state.route.js
+++ src/routes/state.route.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 const stateRouter = Router();
 
-const { getAllStates, getState } = require('../controllers/state.controller');
-
+const {
+  getAllStates,
+  getState,
+  getStateFromZipCode,
+} = require('../controllers/state.controller');
 
 stateRouter.get('/', getAllStates);
 
 stateRouter.get('/:name', getState);
 
+stateRouter.get("/from-zip-code/:zipcode", getStateFromZipCode);
 
 module.exports = { stateRouter };
