6c997b184b56091396a18a131384d7510716a98c
rgraue <rgraue@outlook.com>
2024-05-22 18:56:36 -0700

    all models

---
+++ src/models/state.model.ts
@@ -0,0 +1,37 @@
+export interface StateModel {
+  _id: string;
+  state: string;
+  abbreviation: string;
+  state_emissions: StateEmission[]; 
+  state_energy_costs: StateEnergyCost[];
+  state_energy_breakdown: StateEnergyBreakdown[];
+}
+
+export interface StateEmission {
+  'co2_lbs/mhw': number;
+  'co2_lbs/btu': number;
+  co2_net_emission_estimate: string;
+  date: string;
+}
+
+export interface StateEnergyCost {
+  "usd/btu_electricity": number;
+  "usd/btu_natural_gas": number;
+  "usd/btu_propane": number;
+  "usd/btu_fuel_oil": number;
+  "date": string;
+}
+
+export interface StateEnergyBreakdown {
+  all_fuels: number;
+  natural_gas: number;
+  petroleum: number;
+  coal: number;
+  nuclear: number;
+  hydro: number;
+  wind: number;
+  solar: number;
+  other_renewable: number;
+  date: string;
+  units: string;
+}
---
+++ src/models/zipcode.model.ts
@@ -0,0 +1,28 @@
+export interface ZipcodeModel {
+  _id: string;
+  state: string;
+  zipcode: string;
+  primary_city: string;
+  latitude: number;
+  longitude: number;
+  degree_days: DegreeDay;
+  water_temperature_data: WaterTempDate;
+}
+
+export interface DegreeDay {
+  heating_degree_days: number
+  cooling_degree_days: number
+  site: Site
+}
+
+export interface WaterTempDate {
+  water_temperature: number;
+  site: Site;
+  
+}
+
+interface Site {
+  site_id: string
+  site_name: string
+  station_distance: number
+}
