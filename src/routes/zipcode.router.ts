37cbc1b40c3d4bcc8f70e43e9ec5c30d8461f917
rgraue <rgraue@outlook.com>
2024-05-29 21:15:54 -0700

    init better zipcode

--- src/routes/zipcode.router.ts
+++ src/routes/zipcode.router.ts
@@ -1,5 +1,14 @@
 import { Router } from 'express';
-import { getZipcode } from '../controllers/zipcode.controller';
+import { ZipcodeController } from '../controllers/zipcode.controller';
+import { DynamoService } from '../services/dynamo.service';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DYNAMO_ENDPOINT } from '../constants/routes';
 
-export const zipcodeRouter = Router().get('/:value', getZipcode);
+const dynamoService = new DynamoService(
+  new DynamoDBClient({ region: 'us-west-2', endpoint: DYNAMO_ENDPOINT }),
+);
+
+const controller = new ZipcodeController(dynamoService)
+
+export const zipcodeRouter = Router().get('/:value', controller.getZipcode);
 // .get('/', getAllZipcodes);
